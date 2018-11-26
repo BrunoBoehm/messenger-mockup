a49975a54758cb6bee47fa7a5bbb913343326035
BrunoBoehm <bruno.boehm@gmail.com>
2018-11-26 16:40:02 +0100

    Modularize architecture with import export ES6

---
+++ public/js/chatMessages/chat1.js
@@ -0,0 +1,58 @@
+// Emojis codes from https://afeld.github.io/emoji-css/
+export const chatMessages = [
+    {   // QUESTION
+        msg: "Comment je fais pour avoir plus de clients grâce à internet ?",
+        type: "sent"
+    },{
+        msg: "Salut!",
+        type: "received"
+    }, {
+        msg: `Ah ça c'est notre rayon <i class="em em-muscle"></i>`, 
+        type: "received"
+    },
+    {
+        msg: "thumb",
+        type: "sent"
+    },
+    {
+        msg: "COOL... J'aimerais bien développer mon business avec le web.",
+        type: "sent"
+    },
+    {   // SOLUTION
+        msg: "OK, en bref et en simplifiant la recette c'est :",
+        type: "received"
+    },
+    {
+        msg: "1. Une bonne <b>stratégie</b> (choisir vos priorités et comment tenter de les atteindre)",
+        type: "received"
+    },
+    {
+        msg: "2. Du bon <b>contenu</b> (photos, vidéos, textes, documents) pour bien expliquer ou donner envie !",
+        type: "received"
+    },
+    {
+        msg: '3. Un <b>site</b> ou une plateforme pour "convertir" les personnes intéressées en vrais contacts (futurs clients)',
+        type: "received"
+    },
+    {
+        msg: 'Et surtout de la régularité et un peu de persévérance <i class="em em-zap"></i><i class="em em-yum"></i>',
+        type: "received"
+    },
+    {   // Next Step
+        msg: 'OK... On commence quand?',
+        type: "sent"
+    },
+    {
+        msg: '<i class="em em-tada"></i> Cool! On va <span class="accent--medium">ACTIVER</span> votre présence digitale!',
+        type: "received"
+    },
+    {
+        msg: 'https://media.giphy.com/media/xULW8vuRXQYvaGJAQw/giphy.gif',
+        type: "received"
+    },
+    {
+        msg: 'D\'autres questions ? Contactez-nous<br> - sur le chat <span class="accent">fb.me/lyketil</span><br>- ou sur notre site <span class="accent--strong">lyketil.com</span>',
+        type: "received"
+    },  
+];
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,60 +1,5 @@
-// Emojis codes from https://afeld.github.io/emoji-css/
-const chatMessages = [
-    {   // QUESTION
-        msg: "Comment je fais pour avoir plus de clients grâce à internet ?",
-        type: "sent"
-    },{
-        msg: "Salut!",
-        type: "received"
-    }, {
-        msg: `Ah ça c'est notre rayon <i class="em em-muscle"></i>`, 
-        type: "received"
-    },
-    {
-        msg: "thumb",
-        type: "sent"
-    },
-    {
-        msg: "COOL... J'aimerais bien développer mon business avec le web.",
-        type: "sent"
-    },
-    {   // SOLUTION
-        msg: "OK, en bref et en simplifiant la recette c'est :",
-        type: "received"
-    },
-    {
-        msg: "1. Une bonne <b>stratégie</b> (choisir vos priorités et comment tenter de les atteindre)",
-        type: "received"
-    },
-    {
-        msg: "2. Du bon <b>contenu</b> (photos, vidéos, textes, documents) pour bien expliquer ou donner envie !",
-        type: "received"
-    },
-    {
-        msg: '3. Un <b>site</b> ou une plateforme pour "convertir" les personnes intéressées en vrais contacts (futurs clients)',
-        type: "received"
-    },
-    {
-        msg: 'Et surtout de la régularité et un peu de persévérance <i class="em em-zap"></i><i class="em em-yum"></i>',
-        type: "received"
-    },
-    {   // Next Step
-        msg: 'OK... On commence quand?',
-        type: "sent"
-    },
-    {
-        msg: '<i class="em em-tada"></i> Cool! On va <span class="accent--medium">ACTIVER</span> votre présence digitale!',
-        type: "received"
-    },
-    {
-        msg: 'https://media.giphy.com/media/xULW8vuRXQYvaGJAQw/giphy.gif',
-        type: "received"
-    },
-    {
-        msg: 'D\'autres questions ? Contactez-nous<br> - sur le chat <span class="accent">fb.me/lyketil</span><br>- ou sur notre site <span class="accent--strong">lyketil.com</span>',
-        type: "received"
-    },  
-];
+// const chatMessages = require('./chatMessages/chat1');
+import { chatMessages } from './chatMessages/chat1.js';
 
 $(document).ready(function () {
 
@@ -235,13 +180,13 @@ $(document).ready(function () {
             };
 
             // load box on page, hide spinner after main message delay, fade in message text inside box
-            msgname = "." + obj.name;
+            const msgname = "." + obj.name;
             $(msgname).delay(totalChatDelay).fadeIn(); // appear all div after previous message
 
-            spinner = ".sp-" + obj.name;
+            const spinner = ".sp-" + obj.name;
             $(spinner).delay(chatDelay2).hide(300); // spins during "chatDelay2" time then hides
 
-            msginner = ".message-" + obj.name;
+            const msginner = ".message-" + obj.name;
             $(msginner).delay(chatDelay3).fadeIn(100, function(){
                 $(this).find(".em").addClass("animated pulse infinite");
             }); // text appears just after spinner goes out, and emoji gets animated
